90abbe7ecc670764b859cd56b775664cdd6b9ef4
Ivan Petr <ivan.kiel@gmail.com>
2020-11-18 10:45:40 +0800

    Получение спсика дикторов и задания добавлены

--- app/src/main/java/com/example/azuredemo/Dictor.java
+++ app/src/main/java/com/example/azuredemo/Dictor.java
@@ -1,11 +1,5 @@
 package com.example.azuredemo;
 
 class Dictor {
-    public Dictor(String name, String lang, String sex) {
-        this.name = name;
-        this.lang = lang;
-        this.sex = sex;
-    }
-
-    String name, lang, sex;
+    String ShortName, Gender, Locale;
 }
--- app/src/main/java/com/example/azuredemo/MainActivity.java
+++ app/src/main/java/com/example/azuredemo/MainActivity.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,13 +37,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void onGetDictorsClick (View v) {
+        AzureTask task = new AzureTask();
+        // TODO: проверить, не вышло ли время жизни токена
+        // см. https://docs.microsoft.com/ru-ru/azure/cognitive-services/speech-service/rest-text-to-speech
+
+        Request req = new Request(RequestType.GET_LANGUAGES, bearerToken);
+        task.execute(req);
+
+    }
+
     enum RequestType {
         AUTH, GET_LANGUAGES, MAKE_VOICE
     }
     class Request {
-        public Request(RequestType type, String subscriptionKey) {
+        public Request(RequestType type, String key) {
             this.type = type;
-            this.subscriptionKey = subscriptionKey;
+            switch (type)
+            {
+                // в зависимости от типа запроса по разному трактуется поле key
+                case AUTH: this.subscriptionKey = key; break;
+                case GET_LANGUAGES: this.token = key; break;
+            }
+
         }
 
         // тип обращения: авторизация (получение токена)
@@ -59,7 +79,13 @@ public class MainActivity extends AppCompatActivity {
 
         RequestType type;
         String token;
-        ArrayList<Dictor> dictors;
+        Dictor[] dictors;
+
+        public Response(RequestType type, Dictor[] dictors) {
+            this.type = type;
+            this.dictors = dictors;
+        }
+
         byte[] waveData;
 
     }
@@ -74,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: 1) указать верный URL с учётом значения region в объекте req
                 // TODO: region хранить в strings.xml
                 case AUTH: API_URL = "https://francecentral.api.cognitive.microsoft.com/sts/v1.0/issueToken"; break;
-                case GET_LANGUAGES: API_URL = ""; break;
+                case GET_LANGUAGES: API_URL = "https://francecentral.tts.speech.microsoft.com/cognitiveservices/voices/list"; break;
                 case MAKE_VOICE: API_URL = ""; break;
             }
             try {
@@ -101,26 +127,54 @@ public class MainActivity extends AppCompatActivity {
                         String text = sc.nextLine();
                         Response r = new Response(req.type, text);
                         return r; // возвращаем результат обращения к API
-                    } catch (IOException e) { return new Response(req.type, null); } // в случае ошибки возвразаем вместо текста null
-                case GET_LANGUAGES: break;
-                // TODO: получить список дикторов в виде ArrayList<Dictor>
+                    } catch (IOException e) { return null; } // в случае ошибки возвразаем вместо текста null
+                case GET_LANGUAGES:
+                    urlConnection.setRequestProperty("Authorization", "Bearer " + req.token);
 
+                    try {
+                        stream = urlConnection.getInputStream();
+
+                        Gson gson = new Gson();
+                        InputStreamReader reader = new InputStreamReader(stream);
+                        Dictor[] dictors = gson.fromJson(reader, Dictor[].class);
+
+                        Response r = new Response(req.type, dictors);
+                        return r; // возвращаем результат обращения к API
+                    } catch (IOException e) { return null; }
+                // TODO: получить список дикторов в виде ArrayList<Dictor>
+                /*
+                InputStreamReader reader = new InputStreamReader(stream);
+                Weather weather = gson.fromJson(reader, Weather.class);
+                */
                 case MAKE_VOICE: break;
             }
             // удобно считать JSON из потока
-            /*
-            InputStreamReader reader = new InputStreamReader(stream);
-            Weather weather = gson.fromJson(reader, Weather.class);
-            */
+
 
             return null;
         }
 
         @Override
         protected void onPostExecute(Response response) {
-            Log.d("mytag", "token:" + response.token);
-            bearerToken = response.token;
-            bearerDateTime = new Date();
+            if (response == null)
+            {
+                Log.d("mytag", "response is null");
+                return;
+                // TODO: В Spinner languages добавить список языков
+                // TODO: вывести список имён и полов дикторов для требуемого языка (язык взять из Spinner)
+                // самый простой вариант: в системный журнал
+                // изящный вариант: в отдельный Spinner
+
+            }
+            switch (response.type) {
+                case GET_LANGUAGES: Log.d("mytag", "dictors length:" + response.dictors.length);
+                break;
+                case AUTH: Log.d("mytag", "token:" + response.token);
+                    bearerToken = response.token;
+                    bearerDateTime = new Date();
+                    break;
+            }
+
         }
     }
 }
